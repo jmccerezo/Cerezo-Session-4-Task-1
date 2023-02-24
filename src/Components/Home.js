68fbebba36618162a4629a549e6e88de5075532f
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-02-24 09:06:10 +0800

    change id to className

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -35,7 +35,7 @@ class Home extends Component {
               <div className="col s12 m12">
                 <div className="card blue-grey darken-1">
                   <div className="card-content white-text">
-                    <div id="animeCardContainer">
+                    <div className="animeCardContainer">
                       <div className="animeCardContent">
                         <img src={data.images.jpg.image_url} />
                       </div>
